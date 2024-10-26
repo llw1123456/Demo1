58d27d35f0040f9e2c10e98dbc4a78baf16b4e77
liwei.liu <liwei.liuliu@hotmail.com>
2024-10-27 00:35:37 +0800

    first

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int  res = 0;
+    int a  = 0;
+    int c = 1;
+    int x = a + c;
+    for(int i = 1; i <= 10; i++) {
+        res = res + i;
+    }
+    printf("%d", res);
+    return 0;
+}
